3aed22a9e59fe3d7b10c692d1f6c611145842b5d
Ramandeep Singh (MS/ECI-XC) <Ramandeep.Singh2@in.bosch.com>
2022-06-13 18:01:49 +0530

    Test Commit 1

---
+++ TelCo.ColorCoder/ColorFromPairNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TelCo.ColorCoder
+{
+    internal class ColorFromPairNumber
+    {
+        /// <summary>
+        /// Given a pair number function returns the major and minor colors in that order
+        /// </summary>
+        /// <param name="pairNumber">Pair number of the color to be fetched</param>
+        /// <returns></returns>
+        public static ColorPair GetColorFromPairNumber(int pairNumber)
+        {
+            // The function supports only 1 based index. Pair numbers valid are from 1 to 25
+            int minorSize = ColorMaps.colorMapMinor.Length;
+            int majorSize = ColorMaps.colorMapMajor.Length;
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
+            }
+
+            // Find index of major and minor color from pair number
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+
+            // Construct the return val from the arrays
+            ColorPair pair = new ColorPair()
+            {
+                majorColor = ColorMaps.colorMapMajor[majorIndex],
+                minorColor = ColorMaps.colorMapMinor[minorIndex]
+            };
+
+            // return the value
+            return pair;
+        }
+    }
+}
